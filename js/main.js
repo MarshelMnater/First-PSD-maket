e23fdee88c943762d36cc551b45fa084cdc065ce
MarshelMnater <67753548+MarshelMnater@users.noreply.github.com>
2020-07-10 10:49:26 +0300

    UPD

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.banner__slider').slick({
+        prevArrow:'<button type="button" class="slick-btn slick-prev"><img src="images/prev.svg" alt=""></button>',
+        nextArrow:'<button type="button" class="slick-btn slick-next"><img src="images/next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true,
+    });
+    
+});
